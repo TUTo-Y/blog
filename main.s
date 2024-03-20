c9516680f58d90a41a45911a22c6bcccb66ddcc5
TUTo-Y <2219878724@qq.com>
2024-03-20 19:17:18 +0800

    tmp

---
+++ main.s
@@ -0,0 +1,82 @@
+	.file	"main.c"
+	.intel_syntax noprefix
+	.text
+	.globl	add
+	.type	add, @function
+add:
+.LFB6:
+	.cfi_startproc
+	endbr64
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register 6
+	mov	DWORD PTR -20[rbp], edi
+	mov	DWORD PTR -24[rbp], esi
+	mov	edx, DWORD PTR -20[rbp]
+	mov	eax, DWORD PTR -24[rbp]
+	add	eax, edx
+	mov	DWORD PTR -4[rbp], eax
+	mov	eax, DWORD PTR -4[rbp]
+	pop	rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE6:
+	.size	add, .-add
+	.section	.rodata
+.LC0:
+	.string	"c = %d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB7:
+	.cfi_startproc
+	endbr64
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register 6
+	sub	rsp, 16
+	mov	DWORD PTR -12[rbp], 10
+	mov	DWORD PTR -8[rbp], 20
+	mov	edx, DWORD PTR -8[rbp]
+	mov	eax, DWORD PTR -12[rbp]
+	mov	esi, edx
+	mov	edi, eax
+	call	add
+	mov	DWORD PTR -4[rbp], eax
+	mov	eax, DWORD PTR -4[rbp]
+	mov	esi, eax
+	lea	rax, .LC0[rip]
+	mov	rdi, rax
+	mov	eax, 0
+	call	printf@PLT
+	mov	eax, 0
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE7:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 12.3.0-1ubuntu1~22.04) 12.3.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
+0:
+	.string	"GNU"
+1:
+	.align 8
+	.long	0xc0000002
+	.long	3f - 2f
+2:
+	.long	0x3
+3:
+	.align 8
+4:
