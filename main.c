426686c697930ce4bf0f3219f17a4ae1c532d350
TUTo-Y <2219878724@qq.com>
2024-03-22 20:06:04 +0800

    tmp

--- main.c
+++ main.c
@@ -7,17 +7,6 @@
 
 int main()
 {
-    printf("char = %d\n", sizeof(char));
-    printf("short = %d\n", sizeof(short));
-    printf("int = %d\n", sizeof(int));
-    printf("long = %d\n", sizeof(long));
-    printf("long long = %d\n", sizeof(long long));
-    printf("long int = %d\n", sizeof(long int));
-    printf("long long int = %d\n", sizeof(long long int));
-    printf("long = %d\n", sizeof(long));
-    printf("void* = %d\n", sizeof(void*));
-    printf("float = %d\n", sizeof(float));
-    printf("double = %d\n", sizeof(double));
-    printf("long double = %d\n", sizeof(long double));
+    printf("%d\n", 1 ^ 0);
     return 0;
 }
